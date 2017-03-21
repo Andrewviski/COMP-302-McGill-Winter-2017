c534ea027b692790f707fa6c746dce579e55e4ae
Andre Kaba <andree_kaba93@hotmail.com>
2017-03-20 22:50:02 -0400

    working phew...

--- assignment4/assignment4.fs
+++ assignment4/assignment4.fs
@@ -1,3 +1,6 @@
+(* Assignment 4 *) (* Do not edit this line. *)
+(* Student name: Andree Kaba, Id Number: 260493293 *) (* Edit this line. *)
+
 module Hw4
 type id = string
 
@@ -45,12 +48,14 @@ let rec unify (s : term) (t : term) : substitution =
         
     match (s,t) with
     //two variables we add them to the substition directly 
-    | (Var v1,Var v2) -> if v1= v2 then [] else [(v1,Var v2)]
+    | (Var v1,Var v2) -> []
     //variable and a constant easy TODO: f(x,x) f(3,4)
     | (Var v,Const c) | (Const c,Var v)-> [(v,Const c)]
     //const and another const much match
     | (Const c1,Const c2) -> if c1=c2 then [] else failwith "not unifiable: clashing constants"
+    //we cannot match a function with a constant
     | (Term _,Const _) | (Const _,Term _) -> failwith "not unifiable: term constant clash"
+    //match two functions
     | (Term (f1,args1),Term (f2,args2)) ->
         if f1=f2 && List.length args1 = List.length args2 then
             unify_list (merge (args1,args2))
@@ -66,50 +71,7 @@ and unify_list (s : (term * term) list) : substitution =
     match s with
     | [] -> []
     | (t1,t2)::rest -> 
-        let u_rest=(unify_list rest)
-        let res=unify (apply u_rest t1) (apply u_rest t2)
-        res@u_rest
-                    
-                       
-                   
-(*
-Examples
-> let t1 = Term("f",[Var "x";Var "y"; Term("h",[Var "x"])]);;
-val t1 : term = Term ("f",[Var "x"; Var "y"; Term ("h",[Var "x"])])
-> let t2 = Term("f", [Term("g",[Var "z"]); Term("h",[Var "x"]); Var "y"]);;
-val t2 : term =
-  Term ("f",[Term ("g",[Var "z"]); Term ("h",[Var "x"]); Var "y"])
-> let t3 = Term("f", [Var "x"; Var "y"; Term("g", [Var "u"])]);;
-val t3 : term = Term ("f",[Var "x"; Var "y"; Term ("g",[Var "u"])])
-> unify t1 t2;;
-val it : substitution =
-  [("x", Term ("g",[Var "z"])); ("y", Term ("h",[Var "x"]))]
-> let t4 = Term("f", [Var "x"; Term("h", [Var "z"]); Var "x"]);;
-val t4 : term = Term ("f",[Var "x"; Term ("h",[Var "z"]); Var "x"])
->  let t5 = Term("f", [Term("k", [Var "y"]); Var "y"; Var "x"]);;
-val t5 : term = Term ("f",[Term ("k",[Var "y"]); Var "y"; Var "x"])
-> unify t4 t5;;
-val it : substitution =
-  [("x", Term ("k",[Term ("h",[Var "z"])])); ("y", Term ("h",[Var "z"]))]
-> unify t5 t4;;
-val it : substitution =
-  [("x", Term ("k",[Term ("h",[Var "z"])])); ("y", Term ("h",[Var "z"]))]
-> apply it t4;;
-val it : term =
-  Term
-    ("f",
-     [Term ("k",[Term ("h",[Var "z"])]); Term ("h",[Var "z"]);
-      Term ("k",[Term ("h",[Var "z"])])])
-> let t6 = Term("f", [Const 2; Var "x"; Const 3]);;
-val t6 : term = Term ("f",[Const 2; Var "x"; Const 3])
-> let t7 = Term("f", [Const 2; Const 3; Var "y"]);;
-val t7 : term = Term ("f",[Const 2; Const 3; Var "y"])
-> unify t6 t7;;
-val it : substitution = [("x", Const 3); ("y", Const 3)]
-> apply it t7;;
-val it : term = Term ("f",[Const 2; Const 3; Const 3])
-> unify t1 t7;;
-System.Exception: not unifiable: term constant clash
-....... junk removed .............
-Stopped due to error
-*)
+        //work with the rest
+        let unified_rest=(unify_list rest)
+        //apply the substitution from right to left :)
+        (unify (apply unified_rest t1) (apply unified_rest t2))@unified_rest
